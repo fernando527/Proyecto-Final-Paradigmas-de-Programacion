c873321dda0fbf04cdd26d9efb073dbe68c70237
Darkphel <80648577+fernando527@users.noreply.github.com>
2026-07-21 13:56:58 -0600

    Creacion del MVC
    Creacion del proyecto

---
+++ Proyecto Final Paradigmas de Programacion/Proyecto Final Paradigmas de Programacion/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Final_Paradigmas_de_Programacion.Models;
+using System.Diagnostics;
+
+namespace Proyecto_Final_Paradigmas_de_Programacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Proyecto Final Paradigmas de Programacion/Proyecto Final Paradigmas de Programacion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Final_Paradigmas_de_Programacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
