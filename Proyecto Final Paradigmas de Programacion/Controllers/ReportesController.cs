1ca6c076fdad5e0178f1b0e138f5a74232df16f4
Darkphel <80648577+fernando527@users.noreply.github.com>
2026-07-25 23:47:20 -0600

    formulario crear

--- Proyecto Final Paradigmas de Programacion/Controllers/ReportesController.cs
+++ Proyecto Final Paradigmas de Programacion/Controllers/ReportesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto_Final_Paradigmas_de_Programacion.Models;
 using Proyecto_Final_Paradigmas_de_Programacion.Repositories;
 
 namespace Proyecto_Final_Paradigmas_de_Programacion.Controllers
@@ -12,11 +13,44 @@ namespace Proyecto_Final_Paradigmas_de_Programacion.Controllers
             reporteRepository = new ReporteRepository();
         }
 
+
         public IActionResult Index()
         {
             var reportes = reporteRepository.ObtenerReportes();
 
             return View(reportes);
+        }
+
+
+        // Mostrar formulario
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+
+        // Guardar reporte
+        [HttpPost]
+        public IActionResult Crear(Reporte reporte)
+        {
+
+            if (string.IsNullOrWhiteSpace(reporte.Edificio) ||
+               string.IsNullOrWhiteSpace(reporte.Aula) ||
+               string.IsNullOrWhiteSpace(reporte.Descripcion) ||
+               reporte.IdTipoDanio == 0 ||
+               reporte.IdPrioridad == 0)
+            {
+                ViewBag.Error = "Todos los campos son obligatorios";
+                return View(reporte);
+            }
+
+
+            reporte.IdUsuario = 2;
+
+            reporteRepository.CrearReporte(reporte);
+
+
+            return RedirectToAction("Index");
         }
     }
 }
